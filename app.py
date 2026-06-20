49c937eecd136632a16899050e89bef6d3cb96b3
ayahkahfi1990-cmd <ayahkahfi1990@gmail.com>
2026-06-20 12:55:49 +0700

    Website Game Premium

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return """
-    <h1>Selamat Datang!</h1>
-    <h2>Website Flask Pertama Saya</h2>
-    <p>Website ini online menggunakan Render.</p>
-    """
+def index():
+    return render_template('index.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+@app.route('/labirin')
+def labirin():
+    return render_template('labirin.html')
+
+@app.route('/snake')
+def snake():
+    return render_template('snake.html')
+
+@app.route('/bintang')
+def bintang():
+    return render_template('bintang.html')
+
+@app.route('/luckyspin')
+def luckyspin():
+    return render_template('luckyspin.html')
+
+@app.route('/tebakwarna')
+def tebakwarna():
+    return render_template('tebakwarna.html')
+
+@app.route('/balapan')
+def balapan():
+    return render_template('balapan.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', debug=True)
