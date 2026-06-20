67af3ea0364f55fc17d505f26b23c0c46221ad04
ayahkahfi1990-cmd <ayahkahfi1990@gmail.com>
2026-06-20 13:20:26 +0700

    Update app.py untuk Koyeb

--- app.py
+++ app.py
@@ -34,5 +34,10 @@ def tebakwarna():
 def balapan():
     return render_template('balapan.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 8000))
+    )
